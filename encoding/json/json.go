7bc9c337531938c1f7db96d7f9680918a7b1586a
Mars Zuo <marszuo@foxmail.com>
2022-12-19 22:28:47 +0800

    feat: Add JSON codec to encode and decode messages

---
+++ encoding/json/json.go
@@ -0,0 +1,28 @@
+package json
+
+import (
+	"encoding/json"
+	"google.golang.org/grpc/encoding"
+)
+
+// Name is the name registered for the proto compressor.
+const Name = "json"
+
+func init() {
+	encoding.RegisterCodec(codec{})
+}
+
+// codec is a Codec implementation with protobuf. It is the default codec for gRPC.
+type codec struct{}
+
+func (codec) Marshal(v interface{}) ([]byte, error) {
+	return json.Marshal(v)
+}
+
+func (codec) Unmarshal(data []byte, v interface{}) error {
+	return json.Unmarshal(data, v)
+}
+
+func (codec) Name() string {
+	return Name
+}
